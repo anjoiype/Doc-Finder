de3c190420a437eac82cc4763b5b04ce3e4a03b0
anjo <anjoiype@gmail.com>
2013-10-26 13:33:32 +0530

    new set

--- heroku.php
+++ heroku.php
@@ -15,28 +15,74 @@
    <tbody>
 <?php
 
-$dsn = "pgsql:"
-    . "host=ec2-54-235-74-57.compute-1.amazonaws.com;"
-    . "dbname=d1gueknm6h2psa;"
-    . "user=pwbtzrsrgvgqrq;"
-    . "port=5432;"
-   
-    . "password=AavMrCiPYOhYhVHj173a2tS2EZ";
- 
-$db = new PDO($dsn);
-$query = "SELECT doc_name, loc, feedback"
-    . "FROM search;";
-$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . $row["employee_id"] . "</td>";
-    echo "<td>" . htmlspecialchars($row["doc_name"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["loc"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["feedback"]) . "</td>";
-    echo "</tr>";
+
+// attempt a connection
+
+
+$dbh =  pg_connect("host=ec2-54-235-74-57.compute-1.amazonaws.com port=5432 dbname=d1gueknm6h2psa user=pwbtzrsrgvgqrq password=AavMrCiPYOhYhVHj173a2tS2EZ sslmode=require options='--client_encoding=UTF8'");
+
+
+if (!$dbh) {
+
+
+    die("Error in connection: " . pg_last_error());
+
+
 }
-$result->closeCursor();
+
+
+// execute query
+
+
+$sql = "SELECT * FROM search";
+
+
+$result = pg_query($dbh, $sql);
+
+
+if (!$result) {
+
+
+    die("Error in SQL query: " . pg_last_error());
+
+
+}
+
+
+// iterate over result set
+
+
+// print each row
+
+
+while ($row = pg_fetch_array($result)) {
+
+
+    echo "Country code: " . $row[0] . "<br />";
+
+
+    echo "Country name: " . $row[1] . "<p />";
+
+
+}
+
+
+// free memory
+
+
+pg_free_result($result);
+
+
+// close connection
+
+
+pg_close($dbh);
+
+
 ?>
+
+
+
    </tbody>
   </table>
  </body>
